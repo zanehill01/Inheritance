9bfd6c8ec4e756a9f9222284c5877ae8d59ac652
zanehill01 <zane_hill1@baylor.edu>
2023-02-20 14:15:07 -0600

    updates

---
+++ PersonProgram.py
@@ -0,0 +1,6 @@
+import PersonClass as pc
+
+person = pc.Person('Zane', '23A', '512 788 1279')
+customer = pc.Customer('JJ', '400B', '512-123-5346', '44', True)
+
+pc.print_person(person)
--- f_polymorphism_demo.py
+++ f_polymorphism_demo.py
@@ -2,13 +2,13 @@
 
 import f_animals as animals
 
+
 def main():
     # Create a Mammal object, a Dog object, and
     # a Cat object.
     mammal = animals.Mammal('regular animal')
     dog = animals.Dog()
     cat = animals.Cat()
-
 
     # Display information about each one.
     print('Here are some animals and')
@@ -26,6 +26,15 @@ def main():
 
     cat.show_species()
     cat.make_sound()
+
+
+def show_mammal_info(creature):
+    if isinstance(creature, animals.Mammal):
+        creature.show_species()
+        creature.make_sound()
+    else:
+        print(f'Creature {creature} is not apart of the list')
+
 
 # Call the main function.
 main()
